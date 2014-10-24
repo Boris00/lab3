cc4514adc4a351dd0ca8467b8933157437970fec
Boris <bumatov@gmail.ru>
2014-10-24 12:14:51 +0400

    6

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main()
     double x,y;
 cout<<"Enter two numbers\n"<<endl;
 cin>>x>>y;
-cout<<"Sum of x and y"<<endl<< x+y;
-cout<<"Difference of x and y"<<endl<< x-y;
+cout<<"Sum of x and y"<<endl<< x+y
+    <<"Difference of x and y"<<endl<< x-y
+    <<"X*Y="<<endl<< x/y;
+
 }
